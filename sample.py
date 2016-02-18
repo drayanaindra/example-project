5dae63b2dbb229a2c65d47a1e786c7cefebed55f
M Asep Indrayana <masepindrayana@gmail.com>
2016-02-18 15:51:11 +0700

    add requirements

---
+++ sample.py
@@ -0,0 +1,20 @@
+import falcon
+import json
+from wsgiref import simple_server
+ 
+class QuoteResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': 'I\'ve always been more interested in the future than in the past.',
+            'author': 'Grace Hopper'
+        }
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps(quote)
+ 
+api = falcon.API()
+api.add_route('/quote', QuoteResource())
+
+if __name__ == '__main__':
+    httpd = simple_server.make_server('0.0.0.0', 8041, api)
+    httpd.serve_forever()
